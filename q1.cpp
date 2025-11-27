cecb6c25aabbadc1b32095a7a59dc4961514224c
prawat05 <prawat_be24@thapar.edu>
2025-11-28 01:13:48 +0530

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -3,80 +3,55 @@ order using recursive approach.*/
 #include <iostream>
 #include<vector>
 using namespace std;
-struct Node
+class node //class to make a leaf node
 {
-    int v;
-    Node *l;
-    Node *r;
-    Node(int x) : v(x), l(nullptr), r(nullptr) {}
+    public:
+    int data;
+    node *left;
+    node *right;
+    node(int value)
+    {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
 };
-Node *build(const vector<int> &a)
+void preorder(node *root)
 {
-    if (a.empty() || a[0] == -1)
-        return nullptr;
-    int n = a.size();
-    vector<Node *> nodes(n, nullptr);
-    for (int i = 0; i < n; i++)
-        if (a[i] != -1)
-            nodes[i] = new Node(a[i]);
-    for (int i = 0; i < n; i++)
-        if (nodes[i])
-        {
-            int L = 2 * i + 1, R = 2 * i + 2;
-            if (L < n)
-                nodes[i]->l = nodes[L];
-            if (R < n)
-                nodes[i]->r = nodes[R];
-        }
-    return nodes[0];
+    if (root==nullptr) return;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
 }
-void pre(Node *r, vector<int> &o)
+void inorder(node *root)
 {
-    if (!r)
-        return;
-    o.push_back(r->v);
-    pre(r->l, o);
-    pre(r->r, o);
+    if (root==nullptr) return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
 }
-void ino(Node *r, vector<int> &o)
+void postorder(node *root)
 {
-    if (!r)
-        return;
-    ino(r->l, o);
-    o.push_back(r->v);
-    ino(r->r, o);
-}
-void post(Node *r, vector<int> &o)
-{
-    if (!r)
-        return;
-    post(r->l, o);
-    post(r->r, o);
-    o.push_back(r->v);
+    if (root==nullptr) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
 }
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int n;
-    if (!(cin >> n))
-        return 0;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-    Node *root = build(a);
-    vector<int> p, i, o;
-    pre(root, p);
-    ino(root, i);
-    post(root, o);
-    for (int x : p)
-        cout << x << " ";
-    cout << "\n";
-    for (int x : i)
-        cout << x << " ";
-    cout << "\n";
-    for (int x : o)
-        cout << x << " ";
-    cout << "\n";
-    return 0;
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    cout<<"Tree after preorder"<<endl;
+    preorder(root);
+    cout<<endl<<"Tree after Inorder"<<endl;
+    inorder(root);
+    cout<<endl<<"Tree after postorder"<<endl;
+    postorder(root);
 }
+
+
