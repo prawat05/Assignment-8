3d1f736131644e8cdeb4ec2c458162dea6616361
prawat05 <prawat_be24@thapar.edu>
2025-11-28 09:44:30 +0530

    Update q2a.cpp
    Refactor BST implementation and add search functionality.

--- q2a.cpp
+++ q2a.cpp
@@ -1,163 +1,62 @@
-#include <iostream>
-#include <climits>
+//searching a number in BST
+#include<iostream>
+#include<vector>
 using namespace std;
-struct Node
+class node //class to make a leaf node
 {
-    int v;
-    Node *l;
-    Node *r;
-    Node(int x) : v(x), l(nullptr), r(nullptr) {}
-};
-struct BST
-{
-    Node *root = nullptr;
-    Node *ins(Node *cur, int x)
+    public:
+    int data;
+    node *left;
+    node *right;
+    node(int value)
     {
-        if (!cur)
-            return new Node(x);
-        if (x < cur->v)
-            cur->l = ins(cur->l, x);
-        else if (x > cur->v)
-            cur->r = ins(cur->r, x);
-        return cur;
-    }
-    void insert(int x) { root = ins(root, x); }
-    Node *searchRec(Node *cur, int x)
-    {
-        if (!cur)
-            return nullptr;
-        if (cur->v == x)
-            return cur;
-        return x < cur->v ? searchRec(cur->l, x) : searchRec(cur->r, x);
-    }
-    bool searchIter(int x)
-    {
-        Node *cur = root;
-        while (cur)
-        {
-            if (cur->v == x)
-                return true;
-            cur = x < cur->v ? cur->l : cur->r;
-        }
-        return false;
-    }
-    int maximum()
-    {
-        Node *cur = root;
-        if (!cur)
-            return INT_MIN;
-        while (cur->r)
-            cur = cur->r;
-        return cur->v;
-    }
-    int minimum()
-    {
-        Node *cur = root;
-        if (!cur)
-            return INT_MAX;
-        while (cur->l)
-            cur = cur->l;
-        return cur->v;
-    }
-    int successor(int x)
-    {
-        Node *cur = root;
-        Node *succ = nullptr;
-        while (cur && cur->v != x)
-        {
-            if (x < cur->v)
-            {
-                succ = cur;
-                cur = cur->l;
-            }
-            else
-                cur = cur->r;
-        }
-        if (!cur) return INT_MIN;
-        if (cur->r)
-        {
-            cur = cur->r;
-            while (cur->l)
-                cur = cur->l;
-            return cur->v;
-        }
-        return succ ? succ->v : INT_MIN;
-    }
-    int predecessor(int x)
-    {
-        Node *cur = root;
-        Node *pre = nullptr;
-        while (cur && cur->v != x)
-        {
-            if (x > cur->v)
-            {
-                pre = cur;
-                cur = cur->r;
-            }
-            else
-                cur = cur->l;
-        }
-        if (!cur) return INT_MIN;
-        if (cur->l)
-        {
-            cur = cur->l;
-            while (cur->r)
-                cur = cur->r;
-            return cur->v;
-        }
-        return pre ? pre->v : INT_MIN;
+        data = value;
+        left = nullptr;
+        right = nullptr;
     }
 };
+node* insert_BST(node *root,int val)
+{
+    if(!root) return new node(val);
+    if(root->data<val)
+    {
+        root->right=insert_BST(root->right,val);
+    }
+    else
+    {
+        root->left=insert_BST(root->left,val);
+    }
+    return root;
+}
+node* BST(vector<int> &v)
+{
+    if(!v.size()) return nullptr;
+    node *root = new node(v[0]);
+    for(int i=1;i<v.size();i++)
+    {
+        insert_BST(root,v[i]);
+    }
+    return root;
+}
+void search(node *root , int num)
+{
+    while(root!=NULL && root->data!=num)
+    {
+        if(num<root->data) root = root->left;
+        if(num>root->data) root = root->right;
+    }
+    if(root==NULL) 
+    {
+        cout<<"The given number is not present in the tree"<<endl;
+    }
+    else
+    {
+        cout<<num<<" is present in the tree"<<endl;
+    }
+}
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int m;
-    if (!(cin >> m))
-        return 0;
-    BST t;
-    for (int i = 0; i < m; i++)
-    {
-        int x;
-        cin >> x;
-        t.insert(x);
-    }
-    int Q;
-    cin >> Q;
-    while (Q--)
-    {
-        int type;
-        cin >> type;
-        if (type == 1)
-        {
-            int v;
-            cin >> v;
-            cout << (t.searchRec(t.root, v) ? "Found" : "NotFound") << "\n";
-        }
-        else if (type == 2)
-            cout << t.maximum() << "\n";
-        else if (type == 3)
-            cout << t.minimum() << "\n";
-        else if (type == 4)
-        {
-            int v;
-            cin >> v;
-            int s = t.successor(v);
-            if (s == INT_MIN)
-                cout << "-1\n";
-            else
-                cout << s << "\n";
-        }
-        else if (type == 5)
-        {
-            int v;
-            cin >> v;
-            int p = t.predecessor(v);
-            if (p == INT_MIN)
-                cout << "-1\n";
-            else
-                cout << p << "\n";
-        }
-    }
-    return 0;
+    vector <int> v = {8,3,10,6,14,4,7,13};
+    node *root = BST(v);
+    search(root,15);
 }
